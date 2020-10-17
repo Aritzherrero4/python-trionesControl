12c89aab9166d7c36daf02111bf7c412d83135b5
Aritz Herrero <aritzherrero4@gmail.com>
2020-10-17 12:54:14 +0200

    Add BuiltIn mode and white mode support.

--- trionesControl.py
+++ trionesControl.py
@@ -24,5 +24,23 @@ def setRGB(r: int, g: int, b: int, device):
     payload.append(0x00)
     payload.append(0xF0)
     payload.append(0xAA)
-    a = device.char_write_handle(0x0009, payload)
-  
+    device.char_write_handle(0x0009, payload)
+
+def setWhite(intensity: int, device):
+    payload = bytearray() 
+    payload.append(0x56)
+    payload.append(0x0)
+    payload.append(0x0)
+    payload.append(0x0)
+    payload.append(intensity)
+    payload.append(0x0F)
+    payload.append(0xAA)
+    device.char_write_handle(0x0009, payload)
+
+def setBuiltIn(mode: int, speed: int, device):
+    payload = bytearray() 
+    payload.append(0xBB)
+    payload.append(mode)
+    payload.append(speed)
+    payload.append(0x44)
+    device.char_write_handle(0x0009, payload)  
